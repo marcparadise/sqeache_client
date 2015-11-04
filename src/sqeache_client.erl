47b905af144cc7e3e31957d59b9ca5d7960110f9
Marc A. Paradise <marc.paradise@gmail.com>
2015-11-04 16:08:50 -0500

    mirror sqerl's interface more closely

--- src/sqeache_client.erl
+++ src/sqeache_client.erl
@@ -1,9 +1,15 @@
 -module(sqeache_client).
 
--export([execute/2,
-         execute/3,
-         execute/4,
-         execute/5]).
+-export([
+         % for REPL convenience:
+         exec/1, exec/2,
+         stat/1, stat/2, stat/3, stat/4,
+         sel/1, sel/2, sel/3, sel/4,
+         % actual interface
+         statement/2, statement/3, statement/4, statement/5,
+         select/2, select/3, select/4, select/5,
+         execute/2, execute/3
+        ]).
 
 % The timeout for opening a socket and sending data
 % is kept very low so we can quickly fail over if we
@@ -11,17 +17,54 @@
 -define(COMM_TIMEOUT, 250).
 -define(RECV_TIMEOUT, 10000).
 
+exec(Statement) -> execute(any, Statement).
+exec(Statement, Args) -> execute(any, Statement, Args).
+stat(Statement) -> statement(any, Statement).
+stat(Statement, Args) -> statement(any, Statement, Args).
+stat(Statement, Args, XFN) -> statement(any, Statement, Args, XFN).
+stat(Statement, Args, XFN, XFA) -> statement(any, Statement, Args, XFN, XFA).
 
-execute(DbId, StatementName) ->
-    execute(DbId, StatementName, [], identity, []).
+sel(Statement) -> select(any, Statement).
+sel(Statement, Args) -> select(any, Statement, Args).
+sel(Statement, Args, XF) -> select(any, Statement, Args, XF).
+sel(Statement, Args, XF, XFA) -> select(any, Statement, Args, XF, XFA).
 
-execute(DbId, StatementName, Args) ->
-    execute(DbId, StatementName, Args, identity, []).
 
-execute(DbId, StatementName, Args, XFormName) ->
-    execute(DbId, StatementName, Args, XFormName, []).
 
-execute(DbId, StatementName, Args, XFormName, XFormArgs) ->
+% TODO maybe just use execute and run parse xforms if any locally?
+select(DbId, Statement) ->
+    do(DbId, select, Statement, [], identity, []).
+
+select(DbId, Statement, Args) ->
+    do(DbId, select, Statement, Args, identity, []).
+
+select(DbId, Statement, Args, XFormName) ->
+    do(DbId, select, Statement, Args, XFormName, []).
+
+select(DbId, Statement, Args, XFormName, XFormArgs) ->
+    do(DbId, select, Statement, Args, XFormName, XFormArgs).
+
+statement(DbId, Statement) ->
+    do(DbId, statement, Statement, [], identity, []).
+
+statement(DbId, Statement, Args) ->
+    do(DbId, statement, Statement, Args, identity, []).
+
+statement(DbId, Statement, Args, XFormName) ->
+    do(DbId, statement, Statement, Args, XFormName, []).
+
+statement(DbId, Statement, Args, XFormName, XFormArgs) ->
+    do(DbId, statement, Statement, Args, XFormName, XFormArgs).
+
+
+execute(DbId, Statement) ->
+    do(DbId, execute, Statement, [], none, none).
+
+execute(DbId, Statement, Args) ->
+    do(DbId, excute, Statement, Args, none, none).
+
+
+do(DbId, Type, Statement, Args, XFormName, XFormArgs) ->
 
     % Nope, we're not even pooling or keeping the sockets alive for now..
     % ... one step at a time, let's prove the concept before optimizing...
@@ -30,20 +73,27 @@ execute(DbId, StatementName, Args, XFormName, XFormArgs) ->
     % and (ultimately) terminate or otherwise permit a retry against a differnt host.
     % escon note: support round-robin responses where response type is defined as
     % 'one_of' the resolved value. {data_service_host, etcd_resolver, "/data/service/host", [roundrobin]}
-    %   -
-    % { Addr, Port }  = escon:get_with_watch("data_service_host"), % note we'll need to be a proc for get_with_watch to be helpful...
+    %
+    %% note we'll need to be a proc for get_with_watch to be helpful...
+    % { Addr, Port }  = escon:get_with_watch(data_service_host),
     Addr = {127,0,0,1},
     Port = 6543,
     Sock = gen_tcp:connect(Addr, Port,
                            [binary, {active, false}, {packet, 0},
                             {send_timeout, ?COMM_TIMEOUT}], ?COMM_TIMEOUT),
-    Response = maybe_send_and_receive(Sock, {DbId, StatementName, Args, XFormName, XFormArgs}),
-    gen_tcp:close(Sock),
+    Term = to_term(DbId, Type, Statement, Args, XFormName, XFormArgs),
+    Response = maybe_send_and_receive(Sock, Term),
+    maybe_close(Sock),
     Response.
+
+to_term(DbId, execute, Statement, Args, _, _) ->
+    {DbId, execute, Statement, Args};
+to_term(DbId, Type, Statement, Args, XFormName, XFormArgs) ->
+    {DbId, Type, Statement, Args, XFormName, XFormArgs}.
 
 maybe_send_and_receive({error, Failure}, _Term) ->
     {error, {socket_open_failed, Failure}};
-maybe_send_and_receive(Sock, Term) ->
+maybe_send_and_receive({ok, Sock}, Term) ->
     SendReply = gen_tcp:send(Sock, term_to_binary(Term, [compressed])),
     maybe_receive(Sock, SendReply).
 
@@ -52,16 +102,16 @@ maybe_receive(_Sock, {error, Failure}) ->
 maybe_receive(Sock, _) ->
     reply(gen_tcp:recv(Sock, 0, ?RECV_TIMEOUT)).
 
-reply(ValidResponse) when is_binary(ValidResponse) ->
+reply({ok, ValidResponse}) when is_binary(ValidResponse) ->
     % right now the intent is to get back the raw reply, effectively as
     % sqerl has processed it. We will probably want to wrap that in something
     % meaningful?
     binary_to_term(ValidResponse);
-reply(Error) ->
-    {error, {socket_receive_failed, Error}}.
+reply({error, Failure}) ->
+    {error, {socket_receive_failed, Failure}}.
 
-% Note app  well probably have a layer atop this, eg
-% my_data:execute(StatementName) ->
-%   my_data:execute(my_known_db_identifier, StatementName).
-%
-%
+maybe_close({error, _Any}) ->
+    ok;
+maybe_close({ok, Sock}) ->
+    gen_tcp:close(Sock).
+
