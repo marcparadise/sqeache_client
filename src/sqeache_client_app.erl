0a4f30bd42bee973c8ff0e0d200e79acf7dd9058
Marc Paradise <marc@chef.io>
2015-11-15 02:40:51 -0500

    add pooler support, and pooled persistent connections

--- src/sqeache_client_app.erl
+++ src/sqeache_client_app.erl
@@ -1,4 +1,5 @@
-
+%% ex: ft=erlang ts=4 sw=4 et
+%% -*- erlang-indent-level: 4;indent-tabs-mode: nil; fill-column: 92 -*-
 -module('sqeache_client_app').
 
 -behaviour(application).
@@ -6,6 +7,13 @@
 -export([start/2,stop/1]).
 
 start(_StartType, _StartArgs) ->
-    sqeache_client_sup:start_link().
+    {ok, Pid} = sqeache_client_sup:start_link(),
+    {ok, PoolCfg} = application:get_env(sqeache_client, pool),
+    % Some things we don't want configurable - pool name and start mfa in particular:
+    Unconfigurable =[{start_mfa, {sqeache_client, start_link, []}},
+                     {name, sqp}],
+    {ok, _} = pooler:new_pool(lists:flatten([Unconfigurable, PoolCfg])),
+    {ok, Pid}.
+
 stop(_State) ->
     ok.
--- src/sqeache_client_sup.erl
+++ src/sqeache_client_sup.erl
@@ -1,35 +1,16 @@
-%%%-------------------------------------------------------------------
-%% @doc sqeache_client top level supervisor.
-%% @end
-%%%-------------------------------------------------------------------
-
 -module('sqeache_client_sup').
 
+% Supervisor
 -behaviour(supervisor).
-
-%% API
 -export([start_link/0]).
-
-%% Supervisor callbacks
 -export([init/1]).
 
 -define(SERVER, ?MODULE).
 
-%%====================================================================
-%% API functions
-%%====================================================================
-
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-%%====================================================================
-%% Supervisor callbacks
-%%====================================================================
-
-%% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
-    {ok, { {one_for_all, 0, 1}, []} }.
+    PoolerSup = {pooler_sup, {pooler_sup, start_link, []}, permanent, infinity, supervisor, [pooler_sup]},
+    {ok, {{one_for_one, 5, 10}, [PoolerSup]}}.
 
-%%====================================================================
-%% Internal functions
-%%====================================================================
